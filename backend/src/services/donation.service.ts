b7bf6cd365bfffb72dfa5e1820d3fe8f145e222e
renana <renanazinnar@gmail.com>
2025-10-26 19:48:36 +0200

    complete merge

--- backend/src/services/donation.service.ts
+++ backend/src/services/donation.service.ts
@@ -51,62 +51,62 @@ export default {
   },
 
 
-  creditDonate: async (creditDonation: Omit<CreditDonation, "method">, campaignId:string) => {
+  creditDonate: async (creditDonation: Omit<CreditDonation, "method">, campaignId: string) => {
 
-  try {
-    const campaign = await campaignService.getById(campaignId);
-    if(!campaign?.blockchainTx){
-      throw new ServerError('campaing in insuitable for crypto', 400);
-    }
+    try {
+      const campaign = await campaignService.getById(campaignId);
+      if (!campaign?.blockchainTx) {
+        throw new ServerError('campaing in insuitable for crypto', 400);
+      }
 
-    const {amount, ccNumber, expYear, expMonth, cvv,
+      const { amount, ccNumber, expYear, expMonth, cvv,
         ownerId, ownername, currency,
-        email, firstName, lastName} = creditDonation;
-    const chargeResponse = await fetch('http://localhost:8890/api/charge', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        amount, ccNumber, expYear, expMonth, cvv,
-        ownerId, ownername, currency,
-        email, firstName, lastName
-      }),
-    });
-
-    if (chargeResponse.status === 200) {
-      const data = await chargeResponse.json() as {message:string, charge:number, code:string};
-      
-      const txHash = await blockchainService.recordFiatDonation(+(campaign?.blockchainTx), data.charge, currency, data.code )
-
-      const donation = new Donation({
-        email: creditDonation.email,
-        phone: creditDonation.phone,
-        firstName: creditDonation.firstName,
-        lastName: creditDonation.lastName,
-        campaign: campaignId,
-        amount: +data.charge,
-        currency,
-        method: 'card',
-        txHash:txHash,
-        comment:creditDonation.comment,
+        email, firstName, lastName } = creditDonation;
+      const chargeResponse = await fetch('http://localhost:8890/api/charge', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          amount, ccNumber, expYear, expMonth, cvv,
+          ownerId, ownername, currency,
+          email, firstName, lastName
+        }),
       });
 
-      await donation.save();
-      await campaignService.addDonationToCampaign(campaignId, +data.charge)
+      if (chargeResponse.status === 200) {
+        const data = await chargeResponse.json() as { message: string, charge: number, code: string };
+
+        const txHash = await blockchainService.recordFiatDonation(+(campaign?.blockchainTx), data.charge, currency, data.code)
+
+        const donation = new Donation({
+          email: creditDonation.email,
+          phone: creditDonation.phone,
+          firstName: creditDonation.firstName,
+          lastName: creditDonation.lastName,
+          campaign: campaignId,
+          amount: +data.charge,
+          currency,
+          method: 'card',
+          txHash: txHash,
+          comment: creditDonation.comment,
+        });
+
+        await donation.save();
+        await campaignService.addDonationToCampaign(campaignId, +data.charge)
 
 
-      await AuditLog.create({ action: 'donation_created', meta: { donationId: donation._id } });
+        await AuditLog.create({ action: 'donation_created', meta: { donationId: donation._id } });
 
-      // החזרת תגובה לפרונט
-      return donation ;
-    } else {
-      const text = await chargeResponse.text();
-      throw new ServerError( 'Charge server error: ' + text, 502)
+        // החזרת תגובה לפרונט
+        return donation;
+      } else {
+        const text = await chargeResponse.text();
+        throw new ServerError('Charge server error: ' + text, 502)
+      }
+    } catch (error) {
+      console.error('❌ Error in creditDonate:', error);
+      throw error;
     }
-  } catch (error) {
-    console.error('❌ Error in creditDonate:', error);
-    throw error;
-  }
-},
+  },
   // creates donation record, validates onchain tx if needed, updates campaign.returns donation
   async createDonation({ donorId, campaignId, amount, currency = 'USD', method = 'card', txHash }: any) {
     if (method === 'crypto' && txHash) {
@@ -147,10 +147,29 @@ export default {
   },
 
   async listByNgo(ngoId: string) {
-    return Donation.find().populate({path:'campaign', match:{ngo:ngoId}}).sort({ createdAt: -1 });
-  },
+    const objId = new mongoose.Types.ObjectId(ngoId);
+    return Donation.aggregate([
+      {
+        $lookup: {
+          from: 'campaigns',
+          localField: 'campaign',
+          foreignField: '_id',
+          as: 'cDoc'
+        }
+      },
+      { $unwind: '$cDoc' },
+      {
+        $match: {
+          'cDoc.ngo': objId
+        }
+      }
+      
+    ])
+
+  //return Donation.find().populate({ path: 'campaign', match: { ngo: ngoId } }).sort({ createdAt: -1 });
+},
 
   async listByCampaign(campaignId: string) {
-    return Donation.find({ campaign: campaignId }).populate('donor').sort({ createdAt: -1 });
-  }
+  return Donation.find({ campaign: campaignId }).populate('donor').sort({ createdAt: -1 });
+}
 };
